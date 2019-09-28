ba82c756861f5f90dfe33da1a7b9ad78adc743f5
David Francis <davidjayfrancis@gmail.com>
2019-09-28 10:16:10 -0700

    added axios calls to star wars api and styled components

--- starwars/src/components/StarWars.js
+++ starwars/src/components/StarWars.js
@@ -1,6 +1,7 @@
 import React from "react";
 import StarWarsCard from "./StarWarsCard.js";
 import axios from "axios";
+import "./StarWars.css";
 
 export default function StarWars() {
   const [people, setPeople] = React.useState([]);
@@ -9,16 +10,22 @@ export default function StarWars() {
     async function fetchData() {
       try {
         const peopleList = await axios.get("https://swapi.co/api/people");
-        console.log(peopleList);
+        // .data = obj; .results=array;
+        console.log(peopleList.data.results);
+        setPeople(peopleList.data.results);
       } catch (err) {
         console.log(err);
       }
     }
-  });
+
+    fetchData();
+  }, []);
 
   return (
-    <div>
-      <StarWarsCard />
+    <div className="card-container">
+      {people.map(person => {
+        return <StarWarsCard name={person.name} homeworld={person.homeworld} />;
+      })}
     </div>
   );
 }
--- starwars/src/components/StarWarsCard.js
+++ starwars/src/components/StarWarsCard.js
@@ -1,8 +1,9 @@
 import React from "react";
+import "./StarWars.css";
 
 export default function StarWarsCard(props) {
   return (
-    <div>
+    <div className="card">
       <h1>Name: {props.name}</h1>
       <p>Homeworld: {props.homeworld}</p>
     </div>
