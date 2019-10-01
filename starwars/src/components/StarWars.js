9f19f8d07f9f3ce457d9ba123bbfc169bbc16fc9
David Francis <davidjayfrancis@gmail.com>
2019-09-30 19:53:19 -0700

    refactored to use styled-components instead of component.css file

--- starwars/src/components/StarWars.js
+++ starwars/src/components/StarWars.js
@@ -2,6 +2,16 @@ import React from "react";
 import StarWarsCard from "./StarWarsCard.js";
 import axios from "axios";
 import "./StarWars.css";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  min-width: 960px;
+  max-width: 1200px;
+  justify-content: center;
+  margin: 0 auto;
+`;
 
 export default function StarWars() {
   const [people, setPeople] = React.useState([]);
@@ -10,21 +20,60 @@ export default function StarWars() {
     async function fetchData() {
       try {
         const peopleList = await axios.get("https://swapi.co/api/people");
-        // .data = obj; .results=array;
-        console.log(peopleList.data.results);
+        console.log(peopleList);
+        const promises = peopleList.data.results.map(async person => {
+          try {
+            const planet = await axios.get(person.homeworld);
+            return planet;
+          } catch (err) {
+            console.log(`error getting planet for ${person.name} : ${err}`);
+          }
+        });
+
+        const planets = await Promise.all(promises);
+        planets.map((planet, idx) => {
+          peopleList.data.results[idx].homePlanet = planet
+            ? planet.data
+            : { name: "error retrieving." };
+        });
+
         setPeople(peopleList.data.results);
       } catch (err) {
-        console.log(err);
-      }
-    }
+        console.log(`error getting list of people: ${err}`);
+        throw err;
+      } // try..catch
+    } // function fetchData()
+
     fetchData();
-  }, []);
+  }, []); // useEffect()
 
   return (
-    <div className="card-container">
+    <Container>
       {people.map((person, index) => {
-        return <StarWarsCard name={person.name} height={person.height} />;
+        return (
+          <StarWarsCard
+            name={person.name}
+            homePlanet={person.homePlanet.name}
+          />
+        );
       })}
-    </div>
+    </Container>
   );
 }
+
+/* .card-container {
+  display: flex;
+  flex-wrap: wrap;
+  min-width: 960px;
+  max-width: 1200px;
+  justify-content: center;
+  margin: 0 auto;
+}
+
+.card {
+  width: 300px;
+  margin: 10px;
+  background: rgba(100, 40, 50, 0.7);
+  color: rgb(240, 220, 240);
+  border-radius: 10px;
+} */
--- starwars/src/components/StarWarsCard.js
+++ starwars/src/components/StarWarsCard.js
@@ -1,11 +1,20 @@
 import React from "react";
 import "./StarWars.css";
+import styled from "styled-components";
+
+const Card = styled.div`
+  width: 300px;
+  margin: 10px;
+  background: rgba(100, 40, 50, 0.7);
+  color: rgb(240, 220, 240);
+  border-radius: 10px;
+`;
 
 export default function StarWarsCard(props) {
   return (
-    <div className="card">
+    <Card>
       <h1>Name: {props.name}</h1>
-      <p>Height: {props.height} cm</p>
-    </div>
+      <p>Homeworld: {props.homePlanet}</p>
+    </Card>
   );
 }
